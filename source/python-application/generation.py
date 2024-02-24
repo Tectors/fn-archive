025a2b37e342b2e4e6af203a75f5cb025c51c7b4
Tector <73559984+Tectors@users.noreply.github.com>
2024-02-24 00:18:42 -0800

    Update generation.py

--- source/python-application/generation.py
+++ source/python-application/generation.py
@@ -227,4 +227,4 @@ if possibly_latest_entry:
 
 with open(env_file, "a") as myfile:
     myfile.write(f"version_build={text}{parsed['version'] + '-CL-' + parsed['netcl']}\n")
-    myfile.write(f"additions={_text}")
+    myfile.write(f"additions=\"{_text}\"")
