cc2e8ced09f55066e35d86029b333ab3f3b4f1e8
Tector <73559984+Tectors@users.noreply.github.com>
2024-05-24 16:34:38 -0700

    Update splash.py

--- .github/source/python-application/modules/splash.py
+++ .github/source/python-application/modules/splash.py
@@ -6,7 +6,7 @@ import base64
 # Gets the splash for the game
 def get_splash(version, type="mobileBgImg"):
     # NOTE: Expected to fail again in the future
-    endpoint = 'https://trackercdn.com/ghost/images/2024/3/81032_4270355-fortnitec5s2keyart_en.png'
+    endpoint = 'https://cdn2.unrealengine.com/fortnite-battle-royale-wrecked-social-1920x1080-657551d6cecf.jpg'
     buffer = get(endpoint).content
     
     # Encode and return it
