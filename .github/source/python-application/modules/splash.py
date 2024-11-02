b328c9508bd60f42b96e88a2c930b3812e255578
Tector <73559984+Tectors@users.noreply.github.com>
2024-11-02 11:20:38 -0700

    Update splash.py

--- .github/source/python-application/modules/splash.py
+++ .github/source/python-application/modules/splash.py
@@ -6,7 +6,7 @@ import base64
 # Gets the splash for the game
 def get_splash(version, type="mobileBgImg"):
     # NOTE: Expected to fail again in the future
-    endpoint = 'https://cdn2.unrealengine.com/hero-key-art-1920x1080-2742a939d0f8.jpg'
+    endpoint = 'https://cdn2.unrealengine.com/remix-key-art-1920x1080-0b23bdca2f5f.jpg'
     buffer = get(endpoint).content
     
     # Encode and return it
